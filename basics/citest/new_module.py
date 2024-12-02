963e2fb49ffec724c39df773ec05c5f943a52a40
Tomasz Chmiel <tomasz.chmiel2@edu.uekat.pl>
2024-12-02 10:06:01 +0100

    test ci

---
+++ basics/citest/new_module.py
@@ -0,0 +1,8 @@
+def add(a: int, b: int) -> int:
+    """Adds two numbers."""
+    return a + b
+
+
+def subtract(a: int, b: int) -> int:
+    """Subtracts one number from another."""
+    return a - b
